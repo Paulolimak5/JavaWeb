68310633e4485e7961137070940e5d0f05aa8f7f
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-09-26 20:21:45 -0300

    Adicionado ajax

---
+++ js/Product.js
@@ -0,0 +1,100 @@
+baseUrl = "http://localhost:8080/product";
+
+var pagination;
+function getAllProducts(){
+    $.getJSON(`${baseUrl}/all`, function(data){
+        pagination = {
+            pageNumber: data.pageable.pageNumber,
+            pageSize: data.pageable.pageSize,
+        };
+
+    
+        data.content.forEach(item => {
+            createDom(item);
+        });;
+
+        
+
+    });
+}
+
+
+function createDom(item){
+    let divpai = document.createElement("div");
+    divpai.className = "col-md-3 col-sm-6";
+
+    let divGrid = document.createElement("div");
+    divGrid.className = "product-grid";
+
+    let divImage = document.createElement("div");
+    divImage.className = "product-image";
+
+    let image1 = document.createElement("img");
+    image1.setAttribute("src",`${item.photo}`);
+    image1.className = "pic-1";
+
+    let image2 = document.createElement("img");
+    image2.setAttribute("src",`${item.photo}`);
+    image2.className = "pic-2";
+    
+    let link = document.createElement("a");
+    link.setAttribute('href',`detalhes.html?item=${item.id}`);
+    
+    let ul = document.createElement("ul");
+    ul.className = "social";
+
+    let liName = document.createElement("li");
+
+    
+    let linkUl = document.createElement("a");
+    linkUl.setAttribute('data-tip',"Ver mais");
+    linkUl.setAttribute('href',`detalhes.html?item=${item.id}`);
+
+    let iconVerMais = document.createElement("i");
+    iconVerMais.className = "fa fa-search";
+
+    let divProductContent = document.createElement("div");
+    divProductContent.className = "product-content";
+
+    let title = document.createElement("h3");
+    title.className = "title";
+
+    let name = document.createTextNode(`${item.name}`);
+
+    let linkName = document.createElement("a");
+    linkName.setAttribute('href',`detalhes.html?item=${item.id}`);
+    
+    let divPrice = document.createElement("div");
+    divPrice.className = "price";
+
+    let preco = document.createTextNode(`${item.value}`);
+
+    let linkCarrinho = document.createElement("a")
+    linkCarrinho.setAttribute('href',"carrinho.html");
+
+    let adicionarCarrinho = document.createTextNode("+ Adicionar ao carrinho");
+
+   
+
+    link.appendChild(image1);
+    link.appendChild(image2);
+    ul.append(liName);
+    linkUl.appendChild(iconVerMais);
+    liName.appendChild(linkUl);
+    divImage.appendChild(link);
+    divImage.appendChild(ul);
+    divGrid.appendChild(divImage);
+
+    linkName.appendChild(name);
+    title.appendChild(linkName);
+    divPrice.appendChild(preco);
+    divProductContent.appendChild(title);
+    divProductContent.appendChild(divPrice);
+    divProductContent.appendChild(adicionarCarrinho);
+
+    divGrid.appendChild(divProductContent);
+
+    divpai.appendChild(divGrid);
+    console.log(divpai);
+    $("#produtos").append(divpai);
+}
