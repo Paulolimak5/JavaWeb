743a310bec511b55ae91962c39ce27ae50b17e41
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-10-11 19:52:41 -0300

    Adicionando o assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { NavBarModule } from './shared/components/navbar/navbar.module';
+import { FooterModule } from './shared/components/footer/footer.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NavBarModule,
+    FooterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/footer/footer.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [FooterComponent]
+})
+export class FooterModule { }
