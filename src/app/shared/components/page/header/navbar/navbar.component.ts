c7c456b53ffdb9d1d874d1218148a64ce7df4c4c
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-10-11 23:29:26 -0300

    Arrumando o header

--- src/app/shared/components/page/header/navbar/navbar.component.ts
+++ src/app/shared/components/page/header/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  connected = false;
   constructor() { }
 
   ngOnInit(): void {
