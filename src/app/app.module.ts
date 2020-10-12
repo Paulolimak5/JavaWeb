5fc3092767b3bc1c33abeb582e2b1a928c74fab2
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-10-11 21:48:01 -0300

    Trocand css por scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { NavBarModule } from './shared/components/navbar/navbar.module';
-import { FooterModule } from './shared/components/footer/footer.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NavBarModule,
-    FooterModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
